7c4523fe44c4d39c4afe16ebd9e47a2c33e98c6a
useradm <useradm@d9710003.com>
2025-07-04 13:42:07 +0200

    changed name s3 bucket

--- eks-install/backend/main.tf
+++ eks-install/backend/main.tf
@@ -3,27 +3,26 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "demo-terraform-eks-state-s3-bucket"
+  bucket = "demo-terraform-eks-state-s3-bucket-crg"
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
 
   lifecycle {
     prevent_destroy = false
   }
-}
 
-resource "aws_s3_bucket_versioning" "terraform_state" {
-  bucket = aws_s3_bucket.terraform_state.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
-
-resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" {
-  bucket = aws_s3_bucket.terraform_state.id
-
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
+  tags = {
+    Name = "Terraform State Bucket"
   }
 }
 
